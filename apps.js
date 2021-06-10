789ba8f757be9203d4153660b2d2d9c6c4c48a4b
Ben <bendownton64@hotmail.co.uk>
2021-06-10 18:26:33 +0100

    added second counter to age

--- apps.js
+++ apps.js
@@ -7,3 +7,19 @@ const setBg = () => {
   
   genNew.addEventListener("click", setBg);
   setBg();
+
+
+  
+  const timeContainer = document.getElementById("time-container");
+  const BIRTH_DAY = "07-06-1989";
+  const BIRTH_DAY_DATE = new Date(BIRTH_DAY);
+  const intlNumberFormatter = new Intl.NumberFormat("en-US");
+
+  setInterval(() => {
+    const now = new Date();
+    const difference = Math.floor(
+      (now.getTime() - BIRTH_DAY_DATE.getTime()) / 1000
+    );
+
+    timeContainer.innerText = intlNumberFormatter.format(difference);
+  }, 1000);
